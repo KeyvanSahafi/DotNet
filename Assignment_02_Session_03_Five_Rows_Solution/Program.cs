5d84c6c3004682a6a87fc4c7edde042922f2f6bf
KeyvanSahafi <keyvan.sahafi@gmail.com>
2022-05-23 12:41:09 +0430

    Updated Assignments

---
+++ Assignment_02_Session_03_Five_Rows_Solution/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int column = 1;
+            int row = 1;
+            int counter = 1;
+            while (row <= 5)
+            {
+                while (column <= 9)
+                {
+                    switch (column)
+                    {
+                        case < 5:
+                        counter = ((column % 5) + row);
+                        break;
+                        case > 5:
+                        counter = (10 - column + row);
+                        break;
+                        default:
+                        counter = 5 + row;
+                        break;
+                    }
+                    if (counter <= 5)
+                    {
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.Write(counter - 5);
+                    }
+                    column++;
+                }
+                column = 1;
+                Console.WriteLine();
+                row++;
+            }
+        }
+    }
+}
---
+++ Assignment_02_Session_03_General_Solution/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please Enter The Desired Number of Rows: ");
+            int numberofrows = Convert.ToInt32(Console.ReadLine());
+            int numberofcolumns = ((numberofrows*2) - 1);
+            int column = 1;
+            int row = 1;
+            int counter = 1;
+            while (row <= numberofrows)
+            {
+                while (column <= numberofcolumns)
+                {
+                    if (column < numberofrows)
+                    {
+                        counter = ((column % numberofrows) + row);
+                    }
+                    if (column == numberofrows)
+                    {
+                        counter = numberofrows + row;
+                    }
+                    if (column > numberofrows)
+                    {
+                        counter = ((2*numberofrows) - column + row);
+                    }
+                    if (counter <= numberofrows)
+                    {
+                        Console.Write(" ");
+                    }
+                    else if (counter > numberofrows)
+                    {
+                        Console.Write(counter - numberofrows);
+                    }
+                    column++;
+                }
+                column = 1;
+                Console.WriteLine();
+                row++;
+            }
+        }
+    }
+}
