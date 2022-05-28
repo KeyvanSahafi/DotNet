9aefbe1e39bd432ad276c4e3c45c23e699805935
KeyvanSahafi <keyvan.sahafi@gmail.com>
2022-05-28 20:33:37 +0430

    New Excercises

---
+++ Assignment_03_Session_03_Sum_of_Odds/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please Enter A Number: ");
+            int inputnumber = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            if (inputnumber <= 0)
+                {
+                  Console.WriteLine("Wrong Input - Please Enter a positive number");
+                  return;
+                }
+            if (inputnumber % 2 == 0)
+                {
+                    inputnumber--;
+                }
+                while (inputnumber > 0)
+                {
+                    sum = sum + inputnumber;
+                    inputnumber = inputnumber -2;
+                }
+                Console.WriteLine($"Sum of Odd number up to your input is: {sum}");
+        }
+    }
+}
