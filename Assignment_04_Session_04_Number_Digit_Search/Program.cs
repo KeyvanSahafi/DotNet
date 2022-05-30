910fd91a556231b43fc0c811a94888291860d62e
KeyvanSahafi <keyvan.sahafi@gmail.com>
2022-05-30 22:42:42 +0430

    Assignment 04, Searching a Digit in a Big Number

---
+++ Assignment_04_Session_04_Number_Digit_Search/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a Number: ");
+            string Number = Console.ReadLine();
+            Console.Write("Enter a Digit (0 to 9): ");
+            string Digit = Console.ReadLine();
+            int rep = Search(Digit, Number);
+            Console.WriteLine($"{Digit} Is Repeated {rep} Times in Your Input Number");
+        }
+        public static int Search(string S1, string S2)
+        {
+            int ctr = 0;
+            int rep = 0;
+            int Val = 0;
+            if (S1 == null || S2 == null || S1.Length == 0 || S2.Length == 0)
+            {
+                return 0;
+            }
+            while (!(('1') <= S2[Val] && S2[Val] <= ('9')) && S2[Val] != '.')
+            {
+                Val++;
+                if (S2[Val] == '.' && S1 =="0")
+                {
+                    rep = 1;
+                }
+            }
+            for (ctr = Val; ctr <= (S2.Length - S1.Length); ctr++)
+            {
+                if (S2.Substring(ctr, S1.Length) == S1)
+                {
+                    rep++;
+                }
+            }
+            return rep;
+        }
+    }
+}
---
+++ Assignment_04_Session_04_Number_Digit_Search_Numeric_Solution/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a Number: ");
+            double Number = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter a Digit (0 to 9): ");
+            char Digit = Convert.ToChar(Console.ReadLine());
+            int rep = 0;
+            foreach (char num in Number.ToString())
+            {
+                if (num == Digit)
+                {
+                    rep++;
+                }
+            }
+            Console.WriteLine($"{Digit} Is Repeated {rep} Times in Your Input Number");
+        }
+     }
+}
