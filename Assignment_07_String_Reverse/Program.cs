e04249fc022d19a4e97011fd8af4485af3bf9987
KeyvanSahafi <keyvan.sahafi@gmail.com>
2022-06-14 00:44:22 +0430

    Reverse a String

---
+++ Assignment_07_String_Reverse/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Enter a String: ");
+            string word = System.Console.ReadLine();
+            System.Console.WriteLine($"Reversed string is: {Reverse(word)}"); 
+        }
+        public static string Reverse(string str)
+        {
+            string rev = "";  
+            int ctr = str.Length - 1;
+            while (ctr >= 0)  
+            {  
+                rev = rev + str[ctr];
+                ctr--;  
+            }  
+            return rev;  
+        }
+    }
+}
