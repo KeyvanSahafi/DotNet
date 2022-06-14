3cb507b106efa6477f33ab3833b527a960480d4f
KeyvanSahafi <keyvan.sahafi@gmail.com>
2022-06-14 16:51:29 +0430

    Solve String Reverse by Recursive Function

---
+++ Assignment_07_String_Reverse_Recursive/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Enter a String: ");
+            string word = System.Console.ReadLine();
+            System.Console.WriteLine(Reverse(word));
+        }
+        public static string Reverse(string str)
+        {
+            int ctr = str.Length - 1;
+            if (ctr >= 0)
+                return str[ctr] + Reverse(str.Substring(0, ctr));
+            else
+                return str;
+        }
+    }
+}
