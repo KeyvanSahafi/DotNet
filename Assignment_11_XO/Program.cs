9d0536ca1c718ca4e1258d8e19b9cdd1c1166ce3
KeyvanSahafi <keyvan.sahafi@gmail.com>
2022-07-15 18:49:09 +0430

    Assignment 11(Medium Level) & 12

---
+++ Assignment_11_XO/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+namespace XO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> choices = new List<string>(){"1" , "2", "3", "4", "5", "6", "7", "8", "9"};
+            string OX = "OX";
+            List<int> availablenumbers = new List<int>(){1, 2, 3, 4, 5, 6, 7, 8, 9};
+            int player = 1;
+            int count = 0;
+            System.Console.WriteLine();
+            System.Console.WriteLine("Welcome To XO \n");
+            while (1 == 1)
+            {
+                System.Console.WriteLine("Player1:X  and  Player2:O \n");
+                System.Console.WriteLine($"Player {player} Turn \n \n");
+                PrintMap(choices);
+                int choice = Convert.ToInt32(System.Console.ReadLine());
+                System.Console.WriteLine();
+                if (choice == null || choice < 1 || choice > 9)
+                    System.Console.WriteLine("Wrong Input, Enter A Number Between 1-9");
+                else if (availablenumbers.Contains(choice) == false && (choice > 0 && choice < 10))
+                    System.Console.WriteLine("Wrong Input, Number Already Picked");
+                else
+                {
+                    availablenumbers.Remove(choice);
+                    choices = choices.Select(x => x.Replace($"{choice}", OX.Substring(player %2 , 1))).ToList();
+                    count++;
+                    player++;
+                    if (player % 2 == 1)
+                        player = 1;
+                    else
+                        player = 2;
+                }
+                if (CheckWin(choices) == 0)
+                    continue;
+                else if (CheckWin(choices) == -1)
+                {
+                    PrintMap(choices);
+                    System.Console.WriteLine();
+                    System.Console.WriteLine("Draw, Thanks For Playing");
+                    break;
+                }
+                else if (CheckWin(choices) == 1)
+                {
+                    PrintMap(choices);
+                    System.Console.WriteLine();
+                    System.Console.WriteLine($"Player {((player / 2 - 1) * -1) + 1} Won, Thanks For Playing");
+                    break;
+                }
+            }
+
+        }
+        public static void PrintMap(List<string> choices)
+        {
+            Console.WriteLine($"  {choices.ElementAt(0)}  |  {choices.ElementAt(1)}  |  {choices.ElementAt(2)}  ");
+            Console.WriteLine("-----|-----|-----");
+            Console.WriteLine($"  {choices.ElementAt(3)}  |  {choices.ElementAt(4)}  |  {choices.ElementAt(5)}  ");
+            Console.WriteLine("-----|-----|-----");
+            Console.WriteLine($"  {choices.ElementAt(6)}  |  {choices.ElementAt(7)}  |  {choices.ElementAt(8)}  ");
+            System.Console.WriteLine();;
+        }
+        public static int CheckWin(List<string> choices)
+        {
+            if (choices.ElementAt(0) == choices.ElementAt(1) && choices.ElementAt(1) == choices.ElementAt(2))
+                return 1;
+            else if (choices.ElementAt(3) == choices.ElementAt(4) && choices.ElementAt(4) == choices.ElementAt(5))
+                return 1;
+            else if (choices.ElementAt(6) == choices.ElementAt(7) && choices.ElementAt(7) == choices.ElementAt(8))
+                return 1;
+            else if (choices.ElementAt(0) == choices.ElementAt(3) && choices.ElementAt(3) == choices.ElementAt(6))
+                return 1;
+            else if (choices.ElementAt(1) == choices.ElementAt(4) && choices.ElementAt(4) == choices.ElementAt(7))
+                return 1;
+            else if (choices.ElementAt(2) == choices.ElementAt(5) && choices.ElementAt(5) == choices.ElementAt(8))
+                return 1;
+            else if (choices.ElementAt(0) == choices.ElementAt(4) && choices.ElementAt(4) == choices.ElementAt(8))
+                return 1;
+            else if (choices.ElementAt(2) == choices.ElementAt(4) && choices.ElementAt(4) == choices.ElementAt(6))
+                return 1;
+            else if (choices.All(c=>c=="X" || c=="O") == true)
+                return -1;
+            else
+                return 0;
+        }
+    }
+}
---
+++ Assignment_12_SQL_Questions/Program.cs
@@ -0,0 +1,47 @@
+﻿
+
+/* Q1: First Match
+------------------*/
+
+select SC.country_name from [dbo].[match_details] MD inner join [dbo].[Soccer Country] SC
+on MD.team_id = SC.country_id where MD.match_no = 1
+
+/* Q2: Champion
+------------------*/
+
+select SC.country_name from [dbo].[match_details] MD inner join [dbo].[Soccer Country] SC
+on MD.team_id = SC.country_id where MD.play_stage = 'F' and MD.win_lose = 'W'
+
+/* Q3: Most Viewd Game
+------------------*/
+
+select SC.country_name, MM.audence 'Number of Audience' from [dbo].[match_mast] MM inner join [dbo].[match_details] MD 
+on MM.match_no = MD.match_no inner join [dbo].[Soccer Country] SC
+on SC.country_id = MD.team_id where audence = (Select max(audence) from match_mast)
+
+/* Q4: Portugal Hungary Game Result
+------------------*/
+
+select SC.country_name, MD.goal_score 'Result' from [dbo].[match_mast] MM inner join [dbo].[match_details] MD 
+on MM.match_no = MD.match_no inner join [dbo].[Soccer Country] SC
+on SC.country_id = MD.team_id where EXISTS (select * from match_details where SC.country_id = 1209 or country_id = 1214) and MD.goal_score = 3
+
+/* Q5: Runner-Up 
+------------------*/
+
+select SC.country_name from [dbo].[match_details] MD inner join [dbo].[Soccer Country] SC
+on MD.team_id = SC.country_id where MD.play_stage = 'F' and MD.win_lose = 'L'
+
+/* Q6: Runner-Up 
+------------------*/
+
+select country_name , match_no , goal_score 'Goals', MD.win_lose 'Match Result' from [dbo].[match_details] as MD 
+inner join [dbo].[Soccer Country] as SC
+on MD.team_id = SC.country_id where decided_by = 'N'
+group by country_name , match_no , goal_score, win_lose
+
+/* Q7: Host Country
+------------------*/
+
+select TOP(1) SCC.country_name from [dbo].[Soccer Country] SCC inner join [dbo].[Soccer_city] SC 
+on SCC.country_id = SC.country_id
